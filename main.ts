3be089537d0559212ebe2f283219687bd9d0a26d
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-10-28 12:54:36 -0700

    initial tickevent

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
- 
+
+//% block="TickEvents"
+namespace tickevent {
+
+    export class KeyValue {
+        constructor(
+            public key: string,
+            public value: string | number
+        ) { }
+    }
+
+    //% block="key $key value $value"
+    //% blockId=tickeventcreatekv
+    export function createKV(key: string, value: string | number): KeyValue {
+        return new KeyValue(key, value);
+    }
+
+    //% block="post tick event $tickData"
+    //% blockId=tickeventpost
+    //% tickData.defl=tickeventcreatekv
+    export function post(tickData: KeyValue[]) {
+        const tick: { [index: string]: string | number } = {};
+
+        for (const data of tickData) {
+            tick[data.key] = data.value;
+        }
+
+        const toSend = JSON.stringify(tick);
+        const buf = Buffer.create(toSend.length);
+        for (let i = 0; i < toSend.length; ++i) {
+            buf[i] = toSend.charCodeAt(i);
+        }
+        console.log(buf.toString())
+        sendTick(buf);
+    }
+
+    // List this as an identity function, so the call will get elided in hw compile
+    //% shim=TD_ID
+    function sendTick(msg: Buffer) {
+        control.simmessages.send("tick-event", msg);
+        // TODO: https://github.com/microsoft/pxt-common-packages/pull/1194
+        // control.simmessages.send("tick-event", msg, /** noBroadcast */ true);
+    }
+}
